d81fe86bb5a4ac102262dbe6d487f6885f89d291
Gabe Cupino <gabecupino@Gabes-MacBook-Pro.local>
2017-05-24 18:12:04 -0700

    Implemented writing results to an out file.

--- search.py
+++ search.py
@@ -2,16 +2,18 @@ import json
 
 INDEX_PATH = "index.txt"
 BOOKKEEPING_PATH = "WEBPAGES_CLEAN/bookkeeping.json"
+WRITE_PATH = "query_results.txt"
 
 
 # Prompts user for a query and prints out all the URLs as results of the query
 def run_query_program():
     query = prompt_query()
+    write_query_header(WRITE_PATH, query)
     postings = retrieve_postings(query, INDEX_PATH)
     if (len(postings) > 0):
-        print_urls(BOOKKEEPING_PATH, postings)
+        write_urls_to_file(WRITE_PATH, BOOKKEEPING_PATH, postings)
     else:
-        print "No results found for given query."
+        write_no_results(WRITE_PATH, query)
 
 
 # Prompts user for a query and returns the user input as a string
@@ -50,5 +52,24 @@ def print_urls(bookkeeping_path, postings):
     for posting in postings[:-1]:
         print retrieve_url(bookkeeping_path, convert_posting_format(posting))
 
+def write_query_header(write_path, query):
+    with open(write_path, "a") as results:
+        results.write(("Search results for query: \"" + query + "\".\n\n").encode("utf-8"))
+
+def write_no_results(write_path, query):
+    with open(write_path, "a") as results:
+        results.write(("No results found for query: \"" + query + "\"\n\n\n").encode("utf-8"))
+
+
+def write_urls_to_file(write_path, bookkeeping_path, postings):
+
+    with open(write_path, "a") as results:
+        url_count = 0
+        for posting in postings[:-1]:
+            results.write((retrieve_url(bookkeeping_path, convert_posting_format(posting)) + "\n").encode("utf-8"))
+            url_count += 1
+        results.write(("\nNumber of URLs retrieved: " + str(url_count) + "\n\n\n").encode("utf-8"))
+
 if __name__ == "__main__":
     run_query_program()
+    print "Program terminated."
