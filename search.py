d6d95740e8af20c4c424350fa4acbda4a64d8e1e
Gabe Cupino <gabecupino@Gabes-MacBook-Pro.local>
2017-05-24 18:38:36 -0700

    Added max results functionality.

--- search.py
+++ search.py
@@ -3,6 +3,7 @@ import json
 INDEX_PATH = "index.txt"
 BOOKKEEPING_PATH = "WEBPAGES_CLEAN/bookkeeping.json"
 WRITE_PATH = "query_results.txt"
+MAX_RESULTS = 10
 
 
 # Prompts user for a query and prints out all the URLs as results of the query
@@ -11,7 +12,7 @@ def run_query_program():
     write_query_header(WRITE_PATH, query)
     postings = retrieve_postings(query, INDEX_PATH)
     if len(postings) > 0:
-        write_urls_to_file(WRITE_PATH, BOOKKEEPING_PATH, postings)
+        write_urls_to_file(WRITE_PATH, BOOKKEEPING_PATH, postings, MAX_RESULTS)
     else:
         write_no_results(WRITE_PATH, query)
 
@@ -54,17 +55,20 @@ def print_urls(bookkeeping_path, postings):
     for posting in postings[:-1]:
         print retrieve_url(bookkeeping_path, convert_posting_format(posting))
 
+# Writes to file a header for a query
 def write_query_header(write_path, query):
     with open(write_path, "a") as results:
         results.write(("Search results for query \"" + query + "\":\n\n").encode("utf-8"))
 
+
+# Handles writing to file when no results are queried
 def write_no_results(write_path, query):
     with open(write_path, "a") as results:
         results.write(("No results found for query: \"" + query + "\"\n\n\n").encode("utf-8"))
 
 
-def write_urls_to_file(write_path, bookkeeping_path, postings):
-
+# Write URLs to an out file to save run data
+def write_urls_to_file(write_path, bookkeeping_path, postings, max_results):
     with open(write_path, "a") as results:
         url_count = 0
         for posting in postings[:-1]:
@@ -72,6 +76,8 @@ def write_urls_to_file(write_path, bookkeeping_path, postings):
             if len(url) > 0:
                 results.write((url + "\n").encode("utf-8"))
                 url_count += 1
+                if (url_count >= max_results):
+                    break
         results.write(("\nNumber of URLs retrieved: " + str(url_count) + "\n\n\n").encode("utf-8"))
 
 if __name__ == "__main__":
